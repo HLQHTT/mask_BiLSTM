a40489c1c19c20712081982914b25ced70eedd26
HLQHTT <2323222629@qq.com>
2025-11-26 21:24:11 +0800

    Refactor code in data_generate functions
    Updated data normalization method to use mean and standard deviation instead of min-max scaling.

--- data_generate.py
+++ data_generate.py
@@ -3,9 +3,8 @@ import numpy as np
 from dataset import Dataset, ESOL_Dataset
 from dataset import vocab_generate
 
-vocab = vocab_generate('。/datasets_deep_learning.csv', "SMILES表示")
-# print(len(vocab))
-# print(list(vocab.token_to_idx.items())[:38])
+vocab = vocab_generate('./datasets_deep_learning.csv', "SMILES表示")
+
 
 #提供给模型数据输入的接口，模型里只需调整种子的数字，即可得到不同的训练集与测试集
 def data_generate(seed):
@@ -16,15 +15,15 @@ def data_generate(seed):
     y_train = data_smiles[1].astype('float32').reshape(-1, 1)
     solvent_train = data_smiles[6].astype('int32')
     ref_train = data_smiles[7].astype('int32')
-    y_mean = y_train.mean()
-    y_max = y_train.max()
-    # y_min = y_train.min()
-    y_train = (y_train-y_mean)/y_max
-    # y_train = (y_train-y_min)/(y_max-y_min)
+    mu_train = y_train.mean()
+    std_train = y_train.std()
+   
+    y_train = (y_train - mu_train) / std_train
+    
 
     x_test = data_smiles[2].astype('int32')
     y_test = data_smiles[3].astype('float32').reshape(-1, 1)
-    y_test = (y_test-y_mean)/y_max
+    y_test  = (y_test  - mu_train) / std_train
     solvent_test = data_smiles[8].astype('int32')
     ref_test = data_smiles[9].astype('int32')
 
@@ -35,17 +34,8 @@ def data_generate(seed):
     x_test = np.array(pd.concat([x_test_temp, pd.DataFrame(ref_test)], axis=1)).astype('int32')
 
 
-    # pd.DataFrame(x_train).to_csv('./x_train.csv')
-    # pd.DataFrame(y_train).to_csv('./y_train.csv')
-    # pd.DataFrame(x_test).to_csv('./x_test.csv')
-    # pd.DataFrame(y_test).to_csv('./y_test.csv')
-#pd.DataFrame(train_temp).to_csv('./train_temp.csv')
-
-    # data_smiles[4].to_csv('./train_set.csv')
-    # data_smiles[5].to_csv('./test_set.csv')
     return x_train, y_train, x_test, y_test, y_mean, y_max
 
-#x_train, y_train, x_test, y_test = data_generate(9)
 
 def ESOL_data_generate(seed):
     #ESOL数据集
@@ -66,5 +56,6 @@ def ESOL_data_generate(seed):
     x_test = data_smiles[2].astype('int32')
     y_test = data_smiles[3].astype('float32').reshape(-1, 1)
     y_test = (y_test-y_mean)/y_max
+
 
     return x_train, y_train, x_test, y_test
